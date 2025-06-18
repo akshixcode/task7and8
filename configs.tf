b58daa41cece78f21b51afb883549fa2ea1f4483
akki <akki99@gmail.com>
2025-06-19 01:13:19 +0530

    Initial commit on repo branch

---
+++ configs.tf
@@ -0,0 +1,67 @@
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_ecr_repository" "strapi" {
+  name = "strapi-app"
+}
+resource "aws_ecs_cluster" "main" {
+  name = "strapi-fargate-cluster"
+}
+
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "strapi" {
+  family                   = "strapi-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  container_definitions    = jsonencode([
+    {
+      name      = "strapi"
+      image     = "${aws_ecr_repository.strapi.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 1337
+          hostPort      = 1337
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "strapi" {
+  name            = "strapi-fargate-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.strapi.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = ["subnet-06cb171643ef7801d"] # Replace with your public subnet IDs
+    security_groups  = ["sg-0b0f5c366b5067842"]     # Replace with your security group ID
+    assign_public_ip = true
+  }
+}
---
+++ taskdefinition.tf
@@ -0,0 +1,27 @@
+
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                    = "my-first-task"
+  container_definitions     = <<DEFINITION
+[
+  {
+    "name": "my-first-task",
+    "image": "myfikcle/my-strapi-app:latest",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ],
+    "memory": 512,
+    "cpu": 256,
+    "networkMode": "awsvpc"
+  }
+]
+  DEFINITION
+  requires_compatibilities  = ["EC2"]
+  network_mode              = "awsvpc"
+  memory                    = 512
+  execution_role_arn        = aws_iam_role.ecsTaskExecutionRole.arn
+  cpu                       = 256
+}
